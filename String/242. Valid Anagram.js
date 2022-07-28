9d15d7d5f9133a133b601dea40a4a76d934da48c
ASMAA EL MOKHTARI <elmokhtari.asmaa@gmail.com>
2022-07-28 16:59:25 +0000

    String folder

--- String/242. Valid Anagram.js
+++ String/242. Valid Anagram.js
@@ -1,3 +1,7 @@
+/**
+ *  O(n) time O(1) space
+ *  Link : https://leetcode.com/problems/valid-anagram/
+ */
 var isAnagram = function(s, t) {
     const transform = str => str.split('').sort().join('');
     return transform(s) === transform(t);
